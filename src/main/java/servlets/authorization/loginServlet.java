a54f826b421e33b1b2c287fe16c6c22464d25e87
ndval21 <108537326+NikolozDvali@users.noreply.github.com>
2024-06-20 00:25:28 +0400

    added login/register servlets, jsps and csss

---
+++ src/main/java/servlets/authorization/loginServlet.java
@@ -0,0 +1,32 @@
+package servlets.authorization;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
+
+@WebServlet("/login")
+public class loginServlet extends HttpServlet {
+
+    public void init() throws ServletException {}
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        System.out.println("RECEIVED INFORMATION: " + username + ", " + password);
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/authorization/login.jsp").forward(request, response);
+    }
+
+    public void destroy() {
+        // do nothing.
+    }
+}
